f8653dd96d92b1a28406d2a77ac2877a1b1f642d
Beatriz Paixão <bpaixao@live.com>
2021-01-20 12:40:05 -0300

    update-condicoes

---
+++ Aula11/Ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Eua'
+console.log(`Vivendo em ${pais}`)
+
+if (pais != 'Brasil') { //Condicional dupla
+    console.log('Você é estrangeiro')
+}else {
+    console.log('Você é Brasileiro')
+}
