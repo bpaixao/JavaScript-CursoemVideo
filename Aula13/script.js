d3f7dd053e58408a24c53ea2cf62c2a75129ff76
Beatriz Paixão <55667496+bpaixao@users.noreply.github.com>
2021-02-15 14:24:33 -0300

    update

--- Aula13/script.js
+++ Aula13/script.js
@@ -1,10 +1,24 @@
 function carregar() {
     var msg = document.getElementById('msg')
-    var msg = document.getElementById('imagem')
+    var img = document.getElementById('imagem')
 
     var data = new Date();
     var hora = data.getHours();
 
     msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora > 5 && hora < 12) {
+        //Bom dia
+        img.src = "teste-manha.png"
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = "teste-tarde.png"
+    } else if (hora >= 19 && hora <= 23) {
+        //Boa noite
+        img.src = "teste-noite.png"
+    } else {
+        //Boa madrugada
+        img.src = "teste-noite.png"
+    }
 
 }
