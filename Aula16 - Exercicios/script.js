5ec0f5da2fb22335c6733e6c93d0cacd757d13da
Beatriz Paixão <bpaixao@live.com>
2021-03-01 14:02:05 -0300

    update

--- Aula16 - Exercicios/script.js
+++ Aula16 - Exercicios/script.js
@@ -4,8 +4,28 @@ function adicionandoNum() {
     var tab = document.getElementById('selTab')
 
     //Verifica se tem numero
-    if(num.value.length == 0 || num.value.length > 100) {
-        alert("Por favor digite um numero valido!")
+    if(num.value == 0 || num.value > 100) {
+        alert("Por favor digite um numero válido!")
+    } else {
+        var n = Number(num.value)
+        var vetor = []
+        var c = 1;
+       
+        //contador comeca em 1 e vai até 10
+        while (c <= 1) {
+            
+            vetor.push(n)
+            let item = document.createElement('option')
+            item.text = `Valor ${n} adicionado`
+            item.value = `tab ${c}`
+            //adicionando no html
+            tab.appendChild(item)
+            c++
+        }
+
+       console.log(vetor)
+        
+        
     }
 
 }
