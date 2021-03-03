a6d3e3ceb0fa3d7c130d23496ee7300e5a3b74ee
Beatriz Paixão <bpaixao@live.com>
2021-03-02 22:36:08 -0300

    update

--- Aula16 - Exercicios/script.js
+++ Aula16 - Exercicios/script.js
@@ -27,10 +27,40 @@ function adicionandoNum() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado!`
         lista.appendChild(item)
+        res.innerHTML = ''
     }  else {
         alert("Valor invalido ou já encontrado na lista")
     }
 
     num.value = ""
     num.focus()
+}
+
+function finalizar () {
+    if(valores.length == 0){
+        alert("Adicione valores antes de finalizar!")
+    } else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        var soma = 0;
+        var media = 0
+        
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            } else if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma/total
+  
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O Maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O Menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A Soma dos valores adicionados é ${soma};</p>`
+        res.innerHTML += `<p>A Média dos valores é ${media}</p>`
+    }
 }
